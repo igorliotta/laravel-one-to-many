a9726f4800d795c4b58cd9a2af117b2851ab7ed7
igorliotta <liottaigor@gmail.com>
2024-01-10 18:17:50 +0100

    ADD-migration and seeder for Type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $types = ['Photography', 'Developing', 'Science', 'University', 'Fitness', 'Art'];
+
+        foreach ($types as $type_name) {
+
+            $new_type = new Type();
+
+            $new_type->name = $type_name;
+            $new_type->slug = Str::slug($type_name);
+
+            $new_type->save();
+        }
     }
 }
